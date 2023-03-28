825d5dd409d80e7695187c5c61386050811723e6
Joe Sasson <sassonjoe66@gmail.com>
2023-03-27 20:46:26 -0400

    updated terminal chat UI

--- static/js/demo.js
+++ static/js/demo.js
@@ -2,7 +2,7 @@ document.querySelector('#file').onchange = function() {
     document.querySelector('label[for="file"]').textContent = this.files[0].name;
 };
 
-document.querySelector('form[action="/upload"]').addEventListener('submit', async (event) => {
+document.querySelector('#upload-form').addEventListener('submit', async (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
     const response = await fetch('/upload', {
@@ -12,6 +12,8 @@ document.querySelector('form[action="/upload"]').addEventListener('submit', asyn
     const result = await response.json();
     if (result.success) {
         document.querySelector('#query-container').style.display = 'block';
+        document.querySelector('#upload-form').classList.add('hidden');
+        addMessage('ai', 'Hello, I am your intelligent digital assistant. What would you like to know?');
     } else {
         alert('File not uploaded.');
     }
@@ -20,6 +22,9 @@ document.querySelector('form[action="/upload"]').addEventListener('submit', asyn
 document.querySelector('#query-form').addEventListener('submit', async (event) => {
     event.preventDefault();
     const prompt = document.querySelector('#prompt').value;
+    addMessage('human', prompt);
+    document.querySelector('#prompt').value = '';
+
     const response = await fetch('/query_llm', {
         method: 'POST',
         headers: {
@@ -28,18 +33,13 @@ document.querySelector('#query-form').addEventListener('submit', async (event) =
         body: `prompt=${encodeURIComponent(prompt)}`
     });
     const result = await response.json();
-
-    const messages = document.querySelector('.messages');
-    const userMessage = document.createElement('div');
-    userMessage.classList.add('message');
-    userMessage.innerHTML = `<span class="human">Human:</span> ${prompt}`;
-    messages.appendChild(userMessage);
-
-    const aiMessage = document.createElement('div');
-    aiMessage.classList.add('message');
-    aiMessage.innerHTML = `<span class="ai">AI:</span> ${result.result.response}`;
-    messages.appendChild(aiMessage);
-
-    // Scroll to the bottom of the terminal.
-    document.querySelector('#terminal').scrollTop = document.querySelector('#terminal').scrollHeight;
+    addMessage('ai', result.result.response);
 });
+
+function addMessage(role, text) {
+    const message = document.createElement('div');
+    message.classList.add('message');
+    message.innerHTML = `<span class="${role}">${role === 'ai' ? 'AI' : 'Human'}:</span> ${text}`;
+    document.querySelector('.messages').appendChild(message);
+    document.querySelector('#terminal').scrollTop = document.querySelector('#terminal').scrollHeight;
+}
