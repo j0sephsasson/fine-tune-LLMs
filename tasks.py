487496cdb8ddee093c3f61f1abfd51dbc9f7fe4a
Joe Sasson <sassonjoe66@gmail.com>
2023-04-01 11:34:39 -0400

    updated tasks

--- tasks.py
+++ tasks.py
@@ -1,15 +1,17 @@
 import requests
 import os
 from dotenv import load_dotenv
+from flask import current_app
+from app import app
 from io import BytesIO
 
 load_dotenv()
 
-def process_file(file_obj, redis_key, redis_instance):
+def process_file(file_contents, redis_key):
     api_url = os.getenv('TUNE_URL')
     headers = {"Authorization": os.getenv('AUTH_HEADER')}
 
-    file_obj = BytesIO(file_obj)
+    file_obj = BytesIO(file_contents)
 
     response = requests.post(api_url, headers=headers, files={"input_file": file_obj})
 
@@ -17,7 +19,8 @@ def process_file(file_obj, redis_key, redis_instance):
         response_json = response.json()
         output_key = response_json["output_key"]
 
-        redis_instance.set(redis_key, output_key)  # Store the output_key in Redis using the combined key
+        with app.app_context():
+            current_app.config['SESSION_REDIS'].set(redis_key, output_key)  # Store the output_key in Redis using the combined key
 
         return output_key
     else:
